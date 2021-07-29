ac8eefe8fa215716e525ff818e70dbf75f9f792e
Kevin McDermott <bigkevmcd@gmail.com>
2021-07-29 07:56:22 +0100

    Tidy up the namespaces service.
    This adds doc strings, and tidies the tests in the namespaces service.

--- pkg/namespace/service.go
+++ pkg/namespace/service.go
@@ -8,12 +8,19 @@ import (
 	"github.com/riser-platform/riser-server/pkg/core"
 )
 
+// Service provides functionality for creating and validating namespaces.
 type Service interface {
-	// ValidateDeployable validates that a namespace is deployable. Returns a ValidationError if it is not.
-	ValidateDeployable(namespaceName string) error
-	// EnsureDefaultNamespace ensures that the default namespace has been provisioned. Designed to be used only at server startup.
-	EnsureDefaultNamespace() error
+	// Create creates a new namespace with the provided name in the namespaces
+	// repository.
 	Create(namespaceName string) error
+
+	// EnsureDefaultNamespace ensures that the default namespace has been
+	// provisioned. Designed to be used only at server startup.
+	EnsureDefaultNamespace() error
+
+	// ValidateDeployable validates that a namespace is deployable. Returns a
+	// ValidationError if it is not.
+	ValidateDeployable(namespaceName string) error
 }
 
 type service struct {
@@ -21,45 +28,47 @@ type service struct {
 	environments core.EnvironmentRepository
 }
 
+// NewService creates and returns a new namespace service.
 func NewService(namespaces core.NamespaceRepository, environments core.EnvironmentRepository) Service {
 	return &service{namespaces, environments}
 }
 
+// EnsureDefaultNamespace checks for the default namespace being in the
+// namespaces repository, and if it doesn't exist, it automatically creates it.
 func (s *service) EnsureDefaultNamespace() error {
 	_, err := s.namespaces.Get(core.DefaultNamespace)
-	if err != nil {
-		if err == core.ErrNotFound {
-			return s.Create(core.DefaultNamespace)
-		}
-		return err
+	if err == nil {
+		return nil
 	}
-
-	return nil
+	if err == core.ErrNotFound {
+		return s.Create(core.DefaultNamespace)
+	}
+	return err
 }
 
+// Create creates a namespace in the namespaces repository with the provided
+// name.
 func (s *service) Create(namespaceName string) error {
 	err := s.namespaces.Create(&core.Namespace{Name: namespaceName})
 	if err != nil {
-		return errors.Wrap(err, "error creating namespace")
+		return errors.Wrapf(err, "error creating namespace %q", namespaceName)
 	}
-
 	return nil
 }
 
+// ValidateDeployable returns an error if the provided name does not exist in
+// the namespaces repository.
 func (s *service) ValidateDeployable(namespaceName string) error {
 	_, err := s.namespaces.Get(namespaceName)
-
-	if err == core.ErrNotFound {
-		namespaces, nsListErr := s.namespaces.List()
-		if nsListErr == nil {
-			validNamespaceNames := toNameList(namespaces)
-			return core.NewValidationErrorMessage(fmt.Sprintf("Invalid namespace %q. Must be one of: %s", namespaceName, strings.Join(validNamespaceNames, ", ")))
-		} else {
-			return nsListErr
-		}
+	if err != core.ErrNotFound {
+		return err
 	}
-
-	return err
+	namespaces, nsListErr := s.namespaces.List()
+	if nsListErr != nil {
+		return nsListErr
+	}
+	validNamespaceNames := toNameList(namespaces)
+	return core.NewValidationErrorMessage(fmt.Sprintf("invalid namespace %q. Must be one of: %s", namespaceName, strings.Join(validNamespaceNames, ", ")))
 }
 
 func toNameList(namespaces []core.Namespace) []string {
--- pkg/namespace/service_test.go
+++ pkg/namespace/service_test.go
@@ -9,14 +9,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_Create(t *testing.T) {
+func TestService_Create(t *testing.T) {
 	namespaces := &core.FakeNamespaceRepository{
 		CreateFn: func(namespace *core.Namespace) error {
 			assert.Equal(t, "myns", namespace.Name)
 			return nil
 		},
 	}
-
 	environments := &core.FakeEnvironmentRepository{
 		ListFn: func() ([]core.Environment, error) {
 			return []core.Environment{
@@ -25,7 +24,6 @@ func Test_Create(t *testing.T) {
 			}, nil
 		},
 	}
-
 	svc := &service{namespaces, environments}
 
 	err := svc.Create("myns")
@@ -34,41 +32,38 @@ func Test_Create(t *testing.T) {
 	assert.Equal(t, 1, namespaces.CreateCallCount)
 }
 
-func Test_Create_WhenNamespaceCreateErr(t *testing.T) {
+func TestService_Create_when_namespace_create_err(t *testing.T) {
 	namespaces := &core.FakeNamespaceRepository{
 		CreateFn: func(namespace *core.Namespace) error {
 			return errors.New("test")
 		},
 	}
-
 	svc := &service{namespaces: namespaces}
 
 	err := svc.Create("myns")
 
-	assert.Equal(t, "error creating namespace: test", err.Error())
+	assert.EqualError(t, err, `error creating namespace "myns": test`)
 }
 
-func Test_EnsureDefaultNamespace_ReturnsErr(t *testing.T) {
+func TestService_EnsureDefaultNamespace_returns_err(t *testing.T) {
 	namespaces := &core.FakeNamespaceRepository{
 		GetFn: func(string) (*core.Namespace, error) {
 			return nil, errors.New("test")
 		},
 	}
-
 	svc := &service{namespaces: namespaces}
 
 	err := svc.EnsureDefaultNamespace()
 
-	assert.Equal(t, "test", err.Error())
+	assert.EqualError(t, err, "test")
 }
 
-func Test_EnsureDefaultNamespace_WhenExists_Noop(t *testing.T) {
+func TestService_EnsureDefaultNamespace_when_exists_noop(t *testing.T) {
 	namespaces := &core.FakeNamespaceRepository{
 		GetFn: func(string) (*core.Namespace, error) {
 			return &core.Namespace{}, nil
 		},
 	}
-
 	svc := &service{namespaces: namespaces}
 
 	err := svc.EnsureDefaultNamespace()
@@ -76,14 +71,13 @@ func Test_EnsureDefaultNamespace_WhenExists_Noop(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-func Test_ValidateDeployable_NamespaceExists(t *testing.T) {
+func TestService_ValidateDeployable_NamespaceExists(t *testing.T) {
 	namespaces := &core.FakeNamespaceRepository{
 		GetFn: func(namespaceArg string) (*core.Namespace, error) {
 			assert.Equal(t, "myns", namespaceArg)
 			return &core.Namespace{Name: namespaceArg}, nil
 		},
 	}
-
 	svc := &service{namespaces: namespaces}
 
 	err := svc.ValidateDeployable("myns")
@@ -92,7 +86,7 @@ func Test_ValidateDeployable_NamespaceExists(t *testing.T) {
 	assert.Equal(t, 1, namespaces.GetCallCount)
 }
 
-func Test_ValidateDeployable_NamespaceMissing(t *testing.T) {
+func TestService_ValidateDeployable_namespace_missing(t *testing.T) {
 	namespaces := &core.FakeNamespaceRepository{
 		GetFn: func(namespaceArg string) (*core.Namespace, error) {
 			return nil, core.ErrNotFound
@@ -104,16 +98,15 @@ func Test_ValidateDeployable_NamespaceMissing(t *testing.T) {
 			}, nil
 		},
 	}
-
 	svc := &service{namespaces: namespaces}
 
 	err := svc.ValidateDeployable("myns")
 
 	require.IsType(t, &core.ValidationError{}, err, err.Error())
-	assert.Equal(t, `Invalid namespace "myns". Must be one of: ns1, ns2`, err.Error())
+	assert.EqualError(t, err, `invalid namespace "myns". Must be one of: ns1, ns2`)
 }
 
-func Test_ValidateDeployable_NamespaceMissing_ListError(t *testing.T) {
+func TestService_ValidateDeployable_namespace_missing_list_error(t *testing.T) {
 	namespaces := &core.FakeNamespaceRepository{
 		GetFn: func(namespaceArg string) (*core.Namespace, error) {
 			return nil, core.ErrNotFound
@@ -122,24 +115,22 @@ func Test_ValidateDeployable_NamespaceMissing_ListError(t *testing.T) {
 			return nil, errors.New("test")
 		},
 	}
-
 	svc := &service{namespaces: namespaces}
 
 	err := svc.ValidateDeployable("myns")
 
-	assert.Equal(t, "test", err.Error())
+	assert.EqualError(t, err, "test")
 }
 
-func Test_ValidateDeployable_GetError(t *testing.T) {
+func TestService_ValidateDeployable_get_error(t *testing.T) {
 	namespaces := &core.FakeNamespaceRepository{
 		GetFn: func(namespaceArg string) (*core.Namespace, error) {
 			return nil, errors.New("test")
 		},
 	}
-
 	svc := &service{namespaces: namespaces}
 
 	err := svc.ValidateDeployable("myns")
 
-	assert.Equal(t, "test", err.Error())
+	assert.EqualError(t, err, "test")
 }
